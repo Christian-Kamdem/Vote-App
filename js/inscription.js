ab2d46647f328fc1b8e1e13999e60709ef9ca514
Christian Kamdem <christiankamdem2@gmail.com>
2018-09-14 00:51:49 +0100

    First mapping page

---
+++ js/inscription.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    const nom = getElementById.value("nom");
+    const prenom = getElementById.value("prenom");
+    const email = getElementById.value("email");
+    const datenais = getElementById.value("datenais");
+    const mdp = getElementById.value("mdp");
+    const tel = getElementById.value("tel");
+    const envoyer = getElementById.value("envoyer");
+});
